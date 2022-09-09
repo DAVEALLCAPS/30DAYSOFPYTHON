0e32da90ff71e7e3ec99b812fc0d8d62ac7055bd
DAVEALLCAPS <daveallcaps@gmail.com>
2022-09-09 17:43:33 -0600

    re Level 1 exercises

--- DAY_18/REGEX.py
+++ DAY_18/REGEX.py
@@ -1,45 +1,26 @@
 # ## 💻 Exercises: Day 18
 
 # ### Exercises: Level 1
+import collections
+import re
+
 #  1. What is the most frequent word in the following paragraph?
-# ```py
-#     paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.
-# ```
+paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
+all_words_re = r'(\w+)'
+all_words = re.findall(all_words_re, paragraph, re.IGNORECASE)
+print(collections.Counter(all_words).most_common())
 
-# ```sh
-#     [
-#     (6, 'love'),
-#     (5, 'you'),
-#     (3, 'can'),
-#     (2, 'what'),
-#     (2, 'teaching'),
-#     (2, 'not'),
-#     (2, 'else'),
-#     (2, 'do'),
-#     (2, 'I'),
-#     (1, 'which'),
-#     (1, 'to'),
-#     (1, 'the'),
-#     (1, 'something'),
-#     (1, 'if'),
-#     (1, 'give'),
-#     (1, 'develop'),
-#     (1, 'capabilities'),
-#     (1, 'application'),
-#     (1, 'an'),
-#     (1, 'all'),
-#     (1, 'Python'),
-#     (1, 'If')
-#     ]
-# ```
-
-# 2. The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.
-
-# ```py
-# points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
-# sorted_points =  [-4, -3, -1, -1, 0, 2, 4, 8]
-# distance = 8 -(-4) # 12
-# ```
+# 2. Extract these numbers from this whole text and find the distance between the two furthest particles.
+whole_text = '''The position of some particles on the horizontal x-axis are -1, 2, -4, -3 and -1 in the negative direction, 
+0 at origin, 4 and 8 in the positive direction.'''
+all_digis_re = r'(-?\d)'
+whole_digis = re.findall(all_digis_re, whole_text)
+print('points =', whole_digis)
+whole_digis_ints = [int(i) for i in whole_digis]
+whole_digis_ints.sort()
+print('sorted points =', whole_digis_ints)
+distance_digis = whole_digis_ints[-1] - whole_digis_ints[0]
+print(f'distance = {whole_digis_ints[-1]} - ({whole_digis_ints[0]}) = {distance_digis}')
 
 # ### Exercises: Level 2
 

9d66d1127e548822a874bebe07bbf14966fae1c1
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2022-09-13 17:40:48 -0600

    finish regular expressions

--- DAY_18/REGEX.py
+++ DAY_18/REGEX.py
@@ -43,12 +43,16 @@ print(is_valid_variable('firstname')) # True
 
 # 1. Clean the following text. After cleaning, count three most frequent words in the string.
 
-#     ```py
-#     sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
 
-#     print(clean_text(sentence));
+def clean_text(tx):
+    tx2 = re.sub('([%$@#&;])','',tx)
+    return tx2
+
+print(clean_text(sentence))
+cleaned = re.findall(all_words_re, clean_text(sentence), re.IGNORECASE)
 #     I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher
 #     print(most_frequent_words(cleaned_text)) # [(3, 'I'), (2, 'teaching'), (2, 'teacher')]
 #     ```
-
+print(collections.Counter(cleaned).most_common(10))
 # 🎉 CONGRATULATIONS ! 🎉

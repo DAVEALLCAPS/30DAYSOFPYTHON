1ffed5dd661db8b9115150439c45fd48705f21ab
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2022-09-17 11:54:41 -0600

    start package manager tasks

--- DAY_20/PACKAGEMGR.py
+++ DAY_20/PACKAGEMGR.py
@@ -1,10 +1,21 @@
 
 # ## Exercises: Day 20
-
+import collections
+import requests
 # 1. Read this url and find the 10 most frequent words. 
 romeo_and_juliet = 'http://www.gutenberg.org/files/1112/1112.txt'
+responses = requests.get(romeo_and_juliet)
+
+def m_f_w(fi, num):
+    wordz = fi.split()
+    c = collections.Counter(wordz).most_common(num)
+    return c
+
+print(m_f_w(responses.text, 10))
+
 # 2. Read the cats API and 
 cats_api = 'https://api.thecatapi.com/v1/breeds'
+responses = requests.get(cats_api)
 #  and find :
 #    1. the min, max, mean, median, standard deviation of cats' weight in metric units.
 #    2. the min, max, mean, median, standard deviation of cats' lifespan in years.

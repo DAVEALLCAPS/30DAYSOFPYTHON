bc2cfa2ec308a4fb639c438d9d4ca3acb78de320
DAVEALLCAPS <daveallcaps@gmail.com>
2022-09-20 12:18:14 -0600

    start classes

--- DAY_21/CLASSES_OBJECTS.py
+++ DAY_21/CLASSES_OBJECTS.py
@@ -3,10 +3,24 @@
 
 # ### Exercises: Level 1
 
-# 1. Python has the module called _statistics_ and we can use this module to do all the statistical calculations. However, to learn how to make function and reuse function let us try to develop a program, which calculates the measure of central tendency of a sample (mean, median, mode) and measure of variability (range, variance, standard deviation). In addition to those measures, find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions that do statistical calculations as methods for the Statistics class. Check the output below.
+# 1. Python has the module called _statistics_ and we can use this module to do all the statistical calculations. 
+# However, to learn how to make function and reuse function let us try to develop a program, which calculates the 
+# measure of central tendency of a sample (mean, median, mode) and measure of variability (range, variance, standard deviation).
+# In addition to those measures, find the min, max, count, percentile, and frequency distribution of the sample. 
+# You can create a class called Statistics and create all the functions that do statistical calculations as 
+# methods for the Statistics class. Check the output below.
+class Statistics:
+    def __init__(self, data):
+        self.data = data
 
-# ```py
-# ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+    def count(self):
+        return len(self.data)
+        
+ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+p = Statistics(ages)
+print(p.data)
+print(p.count())
+print(p)
 
 # print('Count:', data.count()) # 25
 # print('Sum: ', data.sum()) # 744

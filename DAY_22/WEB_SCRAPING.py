43bd9cbdf954ce1d8dbb3b44457f43a5576fdec4
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2022-09-23 22:17:51 -0600

    day 22 web scraping

--- DAY_22/WEB_SCRAPING.py
+++ DAY_22/WEB_SCRAPING.py
@@ -1,8 +1,37 @@
 
 # ## 💻 Exercises: Day 22
+import json
+import requests
+from bs4 import BeautifulSoup
 
 # 1. Scrape the following website and store the data as json file(url = 'http://www.bu.edu/president/boston-university-facts-stats/').
-# 1. Extract the table in this url (https://archive.ics.uci.edu/ml/datasets.php) and change it to a json file
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'lxml')
+result = []
+facts_wrap = soup.find_all('div',class_='facts-wrapper')
+categ = soup.find_all('h5')
+result.append(categ)
+    
+print(result)
+
+# 2. Extract the table in this url (https://archive.ics.uci.edu/ml/datasets.php) and change it to a json file
+url = 'https://archive.ics.uci.edu/ml/datasets.php'
+
+response = requests.get(url)
+content = response.content # we get all the content from the website
+soup = BeautifulSoup(content, 'html.parser') # beautiful soup will give a chance to parse
+print(soup.title) # <title>UCI Machine Learning Repository: Data Sets</title>
+print(soup.title.get_text()) # UCI Machine Learning Repository: Data Sets
+print(soup.body) # gives the whole page on the website
+print(response.status_code)
+
+tables = soup.find_all('table', {'cellpadding':'3'})
+# We are targeting the table with cellpadding attribute with the value of 3
+# We can select using id, class or HTML tag , for more information check the beautifulsoup doc
+table = tables[0] # the result is a list, we are taking out data from it
+for td in table.find('tr').find_all('td'):
+    print(td.text)
 # 2. Scrape the presidents table and store the data as json(https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States). The table is not very structured and the scrapping may take very long time.
 
 # 🎉 CONGRATULATIONS ! 🎉

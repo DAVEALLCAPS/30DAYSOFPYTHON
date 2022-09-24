1add26d3c82c7028b9f88272c2645377f7457970
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2022-09-24 12:15:46 -0600

    finish day 23 virtual environments

--- DAY_23/VIRENV.py
+++ DAY_23/VIRENV.py
@@ -3,4 +3,10 @@
 
 # 1. Create a project directory with a virtual environment based on the example given above.
 
+# Created venv/, python -m venv venv
+# added venv/ to .gitignore
+# created FLASK_PROJECT directory, 30DAYSOFPYTHON/FLASK_PROJECT
+# installed flask, pip install flask
+# and froze requirements. pip freeze > requirements.txt
+
 # 🎉 CONGRATULATIONS ! 🎉
